fac532916b07c88a5aae805047ab37711c626567
Eduardo Ocampo <eocampo3@gmail.com>
2025-03-27 15:18:43 -0700

    import union

--- src/aircraft_data_hierarchy/common_base_model.py
+++ src/aircraft_data_hierarchy/common_base_model.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 from pydantic import BaseModel, Field, field_validator, ConfigDict
 
 class NodeNotFoundError(Exception):
